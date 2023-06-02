21eb71c68b826449a890fca35c4c38aa076742bd
Lionel COTE <grcote7@gmail.com>
2023-06-02 18:36:25 +0200

    Feat: Reset project

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import Gc7 from "./components/Gc7.vue";
-import card from "./components/Card.vue";
 </script>
 
 <template>
 
 	<div class="app">
 		
-		<card>
-			<template v-slot:title>
-			<h3>Un Autre Titre de card direct depuis App</h3>
-		</template>
-		</card>
-		
-		<Gc7 msg="Gc7 Card Depuis App" />
-		
-		<HelloWorld msg="HelloWorld: Vite + Vue" />
 	</div>
-
 
 </template>
