88e2e78257701e0612771c419b838b115e795702
Lionel COTE <grcote7@gmail.com>
2023-06-02 18:58:39 +0200

    Feat: Simple component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
-<script setup>
+<script setup lang="ts">
+
+import CustomBtn from './components/CustomBtn.vue'
+
+const title = "My App";
+
 </script>
 
 <template>
-
+	
 	<div class="app">
+		<h1 class="blue">{{ title }}</h1>
+		<p class="justify">
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam sed
+			explicabo possimus cum doloremque culpa fugit, animi at amet libero ullam
+			temporibus quaerat, in deserunt.
+		</p>
 		
+		<CustomBtn actionBtn = "Test !"/>
 	</div>
-
+	
 </template>
---
+++ src/components/CustomBtn.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+	actionBtn: String,
+});
+</script>
+
+<template>
+	My Btn
+	<button @click="console.log('Yes !')">
+		{{ actionBtn ?? 'ooo' }}
+	</button>
+</template>
